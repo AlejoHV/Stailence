b8f0ee1506fb78436d5a5c12122cdee18a066264
DiacriLop <dianacristinaacdc12@gmail.com>
2025-09-24 23:20:53 -0500

    chore: creación inicial del proyecto Spring Boot con dependencias básicas

---
+++ stailence1/src/main/java/co/edu/uceva/stailence1/Stailence1Application.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.stailence1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stailence1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stailence1Application.class, args);
+	}
+
+}
---
+++ stailence1/src/test/java/co/edu/uceva/stailence1/Stailence1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.stailence1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stailence1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
