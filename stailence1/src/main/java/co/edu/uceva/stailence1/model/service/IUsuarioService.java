59fef2f01d2457852e559fc5e2b741520158aba0
Alejandro Hernandez <jahv0125@gmail.com>
2025-10-26 17:29:55 -0500

    feat: endpoint registro de usuario con los parámetros, nombre, correo y contraseña

--- stailence1/src/main/java/co/edu/uceva/stailence1/model/service/IUsuarioService.java
+++ stailence1/src/main/java/co/edu/uceva/stailence1/model/service/IUsuarioService.java
@@ -1,11 +1,14 @@
 package co.edu.uceva.stailence1.model.service;
 
-import co.edu.uceva.stailence1.model.entities.Usuarios;
 import java.util.List;
+
+import co.edu.uceva.stailence1.model.dto.RegistroUsuarioRequest;
+import co.edu.uceva.stailence1.model.entities.Usuarios;
 
 public interface IUsuarioService {
     List<Usuarios> findAll();
     Usuarios save(Usuarios usuario);
     void delete(Long id);
     Usuarios findById(Long id);
+    Usuarios registrar(RegistroUsuarioRequest request);
 }
--- stailence1/src/main/java/co/edu/uceva/stailence1/model/service/UsuarioServiceImpl.java
+++ stailence1/src/main/java/co/edu/uceva/stailence1/model/service/UsuarioServiceImpl.java
@@ -1,17 +1,24 @@
 package co.edu.uceva.stailence1.model.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import co.edu.uceva.stailence1.model.dao.IUsuarioDao;
-import co.edu.uceva.stailence1.model.entities.Usuarios;
-
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import co.edu.uceva.stailence1.model.dao.IUsuarioDao;
+import co.edu.uceva.stailence1.model.dto.RegistroUsuarioRequest;
+import co.edu.uceva.stailence1.model.entities.Usuarios;
 
 @Service
 public class UsuarioServiceImpl implements IUsuarioService {
 
-    @Autowired
-    private IUsuarioDao usuarioDao;
+    private final IUsuarioDao usuarioDao;
+    private final PasswordEncoder passwordEncoder;
+
+    public UsuarioServiceImpl(IUsuarioDao usuarioDao, PasswordEncoder passwordEncoder) {
+        this.usuarioDao = usuarioDao;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public List<Usuarios> findAll() {
@@ -31,5 +38,27 @@ public class UsuarioServiceImpl implements IUsuarioService {
     @Override
     public Usuarios findById(Long id) {
         return usuarioDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public Usuarios registrar(RegistroUsuarioRequest request) {
+        if (!request.getContrasena().equals(request.getConfirmarContrasena())) {
+            throw new IllegalArgumentException("Las contraseñas no coinciden");
+        }
+
+        usuarioDao.findByCorreo(request.getCorreo()).ifPresent(usuario -> {
+            throw new IllegalStateException("Ya existe un usuario registrado con ese correo");
+        });
+
+        Usuarios nuevoUsuario = new Usuarios();
+        nuevoUsuario.setNombre(request.getNombre());
+        nuevoUsuario.setApellido("");
+        nuevoUsuario.setCorreo(request.getCorreo());
+        nuevoUsuario.setContrasena(passwordEncoder.encode(request.getContrasena()));
+        nuevoUsuario.setRol(Usuarios.Rol.cliente);
+        nuevoUsuario.setTipoUsuario(null);
+        nuevoUsuario.setNegocio(null);
+
+        return usuarioDao.save(nuevoUsuario);
     }
 }
